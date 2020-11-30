4a9db32edf0aa96c4414e3a8c8e520442cf4a592
juliobriones <julesc00@hotmail.com>
2020-11-30 04:21:49 -0600

    Removed unused imports

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
-from rest_framework import renderers
 from snippets import views
-from snippets.views import SnippetViewSet, UserViewSet, api_root
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
