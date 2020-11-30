237bfdbe9d80fe9eac3464a6508f77981a09bdbc
juliobriones <julesc00@hotmail.com>
2020-11-30 03:39:36 -0600

    Initial commit.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+A
+tutorial1/urls.py,a/7/a74b08749ab82a8b54a838044007718184bf0f32
+E
+tutorial1/settings.py,1/1/11e66f479359d4895ca6a31d5ac4359f7b93f403
+B
+snippets/models.py,d/e/de55da8f67fd69f7f0e492e6cbe74fdf10e1b966
+G
+snippets/serializers.py,f/3/f3df0482b89a6a1c3e7d2ff486278ac0c0b7a28f
+@
+snippets/urls.py,6/5/6537164db257857d7fa1789b414be5d5feef34d0
+A
+snippets/views.py,8/9/8913730e19d2b0300a9af442dee9901b8bfe7abf
+G
+snippets/permissions.py,1/d/1db91f3f8fbca904ad0958c3d28cd45006c3b9b6
---
+++ snippets/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from rest_framework import renderers
+from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r"snippets", views.SnippetViewSet)
+router.register(r"users", views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
